ab5fdf188b35de47b506a09fae8b757f06e25295
Daniel <onikoladanielwilliams@gmail.com>
2022-07-06 02:09:37 +0000

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,21 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
+ * print_sign - Entry function
+ * @n: character for sign
  * Return: Always 0.
  */
-int main(void)
+int print_sign(int n)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+_putchar('0');
+return (0);
 }
