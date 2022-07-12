90bfcb852f0e95ab8e63aa40f8a74cc6e3a28c58
Daniel <onikoladanielwilliams@gmail.com>
2022-07-12 22:52:21 +0000

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
  */
 void print_rev(char *s)
 {
-    int i;
-    int size = strlen(s);
-    i = size - 1;
-    while (i >= 0)
-    {
-        _putchar(s[i]);
-        i--;
-    }
-    _putchar('\n');
+int i;
+int size = strlen(s);
+i = size - 1;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}   
+_putchar('\n');
 }
