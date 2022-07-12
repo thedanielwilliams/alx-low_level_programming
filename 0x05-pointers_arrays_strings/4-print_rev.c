1bd38bf0fc134f9ba23865e3f0ac3de2f8a451c2
Daniel <onikoladanielwilliams@gmail.com>
2022-07-12 22:49:15 +0000

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
+#include <string.h>
 /**
- * print_rev - prints a string in reverse
- * @s: the used string reference pointer
+ * print_rev - Entry function
+ * @s: pointer to char
  * Return: 0
  */
-
 void print_rev(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-		i++;
-	while (i--)
-		_putchar(s[i]);
-	_putchar('\n');
+    int i;
+    int size = strlen(s);
+    i = size - 1;
+    while (i >= 0)
+    {
+        _putchar(s[i]);
+        i--;
+    }
+    _putchar('\n');
 }
